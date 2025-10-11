9d90a448fff94db7d6f6d22e81130ba1eacd75bf
Nia Wright <nw.niawright@gmail.com>
2025-10-10 21:07:47 -0400

    Define Homepage skeleton. Add styling to Home & navBar elements

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+// -------- IMPORT REACT-ROUTER ELEMENTS --------
+import { Link } from 'react-router'
+
+const Home = () => {
+  return (
+    <>
+      <h1>Welcome to the My Academy Homepage</h1>
+      <h2>Please login as Instructor or Student</h2>
+      <div className="row loginHub">
+        <div className="col">
+          <div className="card">
+            <h3>Instructor Login</h3>
+            <button className="routeBtn">
+              <Link to='/instructor'>Login</Link>
+            </button>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <h3>Student Login</h3>
+            <button className="routeBtn">
+              <Link to='/student'>Login</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+      <Link to='/signup'>Create Account</Link>
+    </>
+  );
+};
+
+export default Home;
