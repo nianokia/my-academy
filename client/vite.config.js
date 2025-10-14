66f4cca4e2f7d932a8f55f4ffb99af4dad432cd0
Nia Wright <nw.niawright@gmail.com>
2025-10-13 22:34:08 -0400

    Add build & server proxy configuration to vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3200',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
