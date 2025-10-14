a0372c1520444e36f09ba66d4c1a9b63db98100c
Nia Wright <nw.niawright@gmail.com>
2025-10-13 22:31:46 -0400

    Import cors and dotenv and improve env variable handling & enhance static file serving

--- server/server.js
+++ server/server.js
@@ -1,14 +1,32 @@
 import express from 'express';
-const app = express();
-const port = import.meta.env.PORT || 3200;
+import cors from 'cors';
+import 'dotenv/config';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+// -------- DEFINE VARIABLES --------
+const app = express();
+const port = process.env.PORT || 3200;
+
+// -------- DEFINE PATH to the index.html in the build folder --------
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const clientDistPath = path.join(__dirname, '..', 'client', 'dist');
+
+// -------- DEFINE APP USES --------
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(clientDistPath)));
 
 app.get('/', (req, res) => {
-    console.log("Hello from GET operation.");
+    res.sendFile(path.join(clientDistPath, 'index.html'));
+
+    console.log('Welcome to My Academy!');
 });
 
 app.listen(port, () => {
     console.log(`My Academy server listening on port ${port}`);
-    console.log(`Open Dev Server => ${import.meta.env.DOMAIN}`)
+    console.log(`Open Dev Server => ${process.env.DOMAIN}`);
 })
+
+export default app;
