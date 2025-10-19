8683effb8357001ef21503b1335e78a28368f5b6
Nia Wright <nw.niawright@gmail.com>
2025-10-18 22:35:16 -0400

    Update Vite config to load environment variables for API proxy target

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    outDir: 'dist'
-  },
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3200',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  // Load all .env variables from the current working directory and environment (dev or prod)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    build: {
+      outDir: 'dist'
+    },
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_DOMAIN,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  };
 });
