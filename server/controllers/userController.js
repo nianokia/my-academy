ebc98f71a2f17e6e4497fe6d410371989954634f
Nia Wright <nw.niawright@gmail.com>
2025-10-24 06:55:16 -0400

    Add delete User function to server files

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,3 +37,26 @@ export const updateUser = async (req, res) => {
         res.status(500).json({ message: 'Error updating user: ', error: err.message });
     }
 };
+
+// -------- DELETE USER CONTROLLER --------
+export const deleteUser = async (req, res) => {
+   const userId = req.params.id;
+
+    // --- Authenticate current userId to grant permission to delete ---
+   if (req.user.userId !== userId) {
+    return res.status(403).json({ message: "Unauthorized to delete this user." });
+   }
+
+   try {
+       const user = await User.findByPk(userId);
+       if (!user) {
+           return res.status(404).json({ message: 'User not found' });
+       }
+
+        // --- Delete user from DB ---
+       await user.destroy();
+       res.status(200).json({ message: 'User deleted successfully' });
+   } catch (err) {
+       res.status(500).json({ message: 'Error deleting user', error: err.message });
+   }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { updateUser } from '../controllers/userController.js';
+import { updateUser, deleteUser } from '../controllers/userController.js';
 import verifyToken from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // -------- UPDATE USER PROFILE ROUTE --------
 router.put('/:id', verifyToken, updateUser);
+
+// -------- DELETE USER ROUTE --------
+router.delete('/:id', verifyToken, deleteUser);
 
 export default router;
