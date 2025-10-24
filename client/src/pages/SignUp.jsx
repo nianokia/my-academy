1df200565ffa523903b35323ab8edb101439d25b
Nia Wright <nw.niawright@gmail.com>
2025-10-23 20:43:14 -0400

    Show respective sign up modal onClick. Redirect to respective dashboard on successful registration

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,38 @@
-// -------- IMPORT REACT-ROUTER ELEMENTS --------
-import { Link } from 'react-router'
+// ------- IMPORTS --------
+import { useState } from 'react';
+import InstructorRegister from '../components/InstructorRegister';
+import StudentRegister from '../components/StudentRegister';
+import Modal from '../components/Modal';
+import { useNavigate } from 'react-router'
 
 const SignUp = () => {
+  const [role, setRole] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+
+  // --- Handle role selection & open modal on click ---
+  const handleClick = (selectedRole) => {
+    setRole(selectedRole);
+    setIsModalOpen(true);
+  };
+
+  // --- Close modal & reset role ---
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setRole('');
+  };
+
+  // --- Handle successful registration and redirect based on role ---
+  const handleSuccessfulRegistration = (role) => {
+    setIsModalOpen(false);
+    setRole('');
+    if (role === 'student') {
+      navigate('/student');
+    } else if (role === 'instructor') {
+      navigate('/instructor');
+    }
+  };
+
   return (
     <>
       <h1>Sign Up</h1>
@@ -9,20 +40,27 @@ const SignUp = () => {
         <div className="col">
           <div className="card">
             <h3>Instructor Registration</h3>
-            <button className="routeBtn">
-              <Link to='/instructor'>Create Account</Link>
+            <button className="routeBtn" onClick={() => handleClick('instructor')}>
+              Create Account
             </button>
           </div>
         </div>
         <div className="col">
           <div className="card">
             <h3>Student Registration</h3>
-            <button className="routeBtn">
-              <Link to='/student'>Create Account</Link>
+            <button className="routeBtn" onClick={() => handleClick('student')}>
+              Create Account
             </button>
           </div>
         </div>
       </div>
+
+      {/* -------- MODAL FOR REGISTRATION -------- */}
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        {/* --- Render the appropriate registration form based on the selected role --- */}
+        {role === 'instructor' && <InstructorRegister onSuccess={handleSuccessfulRegistration} />}
+        {role === 'student' && <StudentRegister onSuccess={handleSuccessfulRegistration} />}
+      </Modal>
     </>
   );
 };
