b48009e4363a1ea85f130d44ec2243342add2236
Nia Wright <nw.niawright@gmail.com>
2025-10-24 05:59:27 -0400

    Create api/user file and define updateUser function to get proper response

---
+++ client/src/api/user.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// -------- DEFINE USER API BASE URL --------
+const API_URL = `${import.meta.env.VITE_DOMAIN}/api/user`;
+console.log('API URL:', API_URL);
+
+export const updateUser = async (userId, updatedData, token) => {
+    const res = await axios.put(
+        `${API_URL}/${userId}`,
+        updatedData, {
+            headers: { Authorization: `Bearer ${token}` },
+        }
+    );
+    return res.data;
+};
