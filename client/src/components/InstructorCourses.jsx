1de1af9bbafbd36a64aad9d02dcb5d3dba275b59
Nia Wright <nw.niawright@gmail.com>
2025-10-25 11:30:13 -0400

    Use getUserCourses to display Instructor Courses in a list

---
+++ client/src/components/InstructorCourses.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import { BackButton } from "../constants/constants";
+import { fetchUserCourses } from "../api/course";
+
+const InstructorCourses = ({ userId }) => {
+  const [courses, setCourses] = useState([]);
+
+  const { token } = useContext(AuthContext);
+
+  // --- Define the API route using the environment variable ---
+  const tableRoute = `${import.meta.env.VITE_DOMAIN}/api/courses`;
+  console.log('Table Route:', tableRoute);
+  
+  // -------- FETCH ALL COURSES --------
+  const getInstructorCourses = async () => {
+    try {
+      const response = await fetchUserCourses(userId, token);
+      console.log('Fetched Instructor\'s courses:', response);
+      
+      setCourses(response);
+    } catch (err) {
+      console.error('Error fetching instructor\'s courses:', err);
+      alert("Failed to load instructor's courses. Please try again.")
+    }
+  };
+    
+  // --- Call getInstructorCourses whenever the tableRoute changes ---
+  useEffect(() => {
+    if (token) getInstructorCourses();
+  }, [token]);
+
+  return (
+    <>
+      <BackButton />
+      <h1>My Courses</h1>
+      <ul style={{ textAlign: 'start' }}>
+        {courses.map((course) => (
+          <li key={course.id} value={course.id}>
+            <strong>{course.name}</strong>
+            <ul>
+              <li>Credits: {course.credits}</li>
+              <li>Enrollment Limit: {course.enrollment_limit}</li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+      {/* --- Display "No users found" if there are no courses corresponding to instructor --- */}
+      {courses.length === 0 && <p>No courses found.</p>}
+    </>
+  );
+};
+
+export default InstructorCourses;
