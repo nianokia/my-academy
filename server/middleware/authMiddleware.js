d97d170af36969b1d713d7a4d8d735f53d245242
Nia Wright <nw.niawright@gmail.com>
2025-10-25 08:50:16 -0400

    Add logs to middleware for easier debugging

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,14 +2,30 @@ import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
+
     if (!token) {
         return res.status(401).json({ error: 'No token provided' });
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        console.log("Decoded JWT payload: ", decoded);
+
+        const userId = decoded.userId;
+        // const userId = decoded?.id ?? decoded?.userId ?? decoded?.sub ?? decoded?.user?.id ?? null;
+
+        // if (!userId) return res.status(401).json({ error: "Token missing user identifier" });
+
+        req.user = {
+            userId: userId,
+            role: decoded.role,
+            // role: decoded?.role ?? decoded?.user?.role ?? null,
+        };
+
+        console.log("req.user: ", req.user);
+
         next();
     } catch (err) {
+        console.error("Token verification failer: ", err.message);
         return res.status(401).json({ error: 'Invalid token' });
     }
 };
