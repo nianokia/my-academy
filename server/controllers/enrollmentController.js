06bc198b1652d89b874b6819054b0012b3d3ef05
Nia Wright <nw.niawright@gmail.com>
2025-10-26 08:25:13 -0400

    S: Validate prereqs and seat availability before enrolling students

--- server/controllers/enrollmentController.js
+++ server/controllers/enrollmentController.js
@@ -1,6 +1,6 @@
 // import User from "../models/User.js";
 // import Course from "../models/Course.js";
-import { User, Course } from "../models/associations.js";
+import { User, Course, Enrollment } from "../models/associations.js";
 
 // ------------ READ OPERATIONS ------------
 // -------- GET ENROLLED STUDENTS FOR COURSE --------
@@ -27,15 +27,62 @@ export const enrollStudents = async (req, res) => {
         // --- collect array of ids ---
         const { studentIds } = req.body;
 
-        const course = await Course.findByPk(courseId);
+        // --- collect course data including prereqs ---
+        const course = await Course.findByPk(courseId, {
+            include: [{ association: "prerequisites" }],
+        });
         if (!course) return res.status(404).json({ message: "Course not found" });
+
+        // -------- CALCULATE SEAT AVAILABILITY --------
+        // --- count # 'enrolled' statuses linked to the courseId ---
+        const enrolledCount = await Enrollment.count({
+            where: { course_id: courseId, status: "enrolled" },
+        });
+
+        // --- if selected students surpass available seats then send message ---
+        if (enrolledCount + studentIds.length > course.enrollment_limit){
+            return res.status(400).json({
+                message: `Course full: ${course.enrollment_limit - enrolledCount} seats left`,
+            })
+        }
+
+        // -------- VERIFY PREREQUISITES --------
+        // --- array to list unqualified students ---
+        const unqualified = [];
+
+        for (const studentId of studentIds) {
+            // --- collect all prereq ids for the course ---
+            const prereqIds = course.prerequisites.map((prereq) => prereq.prequisite_course_id);
+
+            if (prereqIds.length > 0) {
+                // --- verify if student completed all prereqs ---
+                const completed = await Enrollment.findAll({
+                    where: { student_id: studentId, course_id: prereqIds, status: "enrolled" }
+                });
+                
+                // --- if student has less prereqs completed than required then add them to unqualified array ---
+                if (completed.length < prereqIds.length) unqualified.push(studentId);
+            }
+        }
+
+        // --- notify user that some of the selected students are not qualified to enroll ---
+        if (unqualified.length > 0) {
+            return res.status(400).json({
+                message: "Some students do not have all the prerequisites",
+                unqualified,
+            });
+        }
+
+        // -------- PROCEED WITH ENROLLMENT --------
 
         // --- collect detailed list of all identified students ---
         const students = await User.findAll({ where: { id: studentIds } });
         if (!students) return res.status(404).json({ message: "No student(s) found" });
 
+        const selectedCourse = await Course.findByPk(courseId);
+
         // --- addStudents is derived from Course.belongsToMany association ---
-        await course.addStudents(students);
+        await selectedCourse.addStudents(students, { through: { status: "enrolled" } });
 
         res.status(200).json({ message: "Student(s) enrolled successfully" });
     } catch (err) {
