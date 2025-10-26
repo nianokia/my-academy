d9b6a0bf9ed86436308766ef0a4d016a3106fc51
Nia Wright <nw.niawright@gmail.com>
2025-10-26 05:41:50 -0400

    C: Enroll one or multiple students & dynamically update student list

--- client/src/components/InstructorEnrollments.jsx
+++ client/src/components/InstructorEnrollments.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useContext } from "react";
-import { fetchEnrolledStudents } from "../api/enrollment";
+import { fetchEnrolledStudents, enrollStudents } from "../api/enrollment";
 import AuthContext from "../context/AuthContext";
 import { fetchUsers } from "../api/user";
 
@@ -7,7 +7,9 @@ const InstructorEnrollments = ({ courseId }) => {
   const { token } = useContext(AuthContext);
   const [students, setStudents] = useState([]);
   const [enrolled, setEnrolled] = useState([]);
+  const [selected, setSelected] = useState([]);
 
+  // -------- FETCH STUDENTS --------
   const fetchStudents = async () => {
     const allUsers = await fetchUsers(token);
     const enrolledStudents = await fetchEnrolledStudents(courseId, token);
@@ -29,6 +31,21 @@ const InstructorEnrollments = ({ courseId }) => {
     fetchStudents();
   }, [courseId]);
 
+  // -------- ENROLL STUDENTS --------
+  const handleEnroll = async () => {
+    await enrollStudents(courseId, selected, token);
+    alert("Students enrolled succesfully");
+    setSelected([]);
+    fetchStudents();
+  };
+
+  const toggleSelection = (id) => {
+    // --- if previous includes specified student.id then append it to the selected array, if not filter it out ---
+    setSelected((prev) => prev.includes(id) ? prev.filter(selected => selected !== id) : [...prev, id]);
+  }
+
+  console.log("Selected: ", selected);
+
   return (
     <div className="InstructorEnrollments">
       <h2>Instructor Enrollments</h2>
@@ -41,14 +58,19 @@ const InstructorEnrollments = ({ courseId }) => {
         ))}
       </ul>
       <h3>Available Students</h3>
-      <ul style={{ textAlign: 'start' }}>
+      <ul style={{ textAlign: 'start', listStyle: 'none', margin: '0', padding: '0' }}>
         {students.map(student => (
           <li key={student.id}>
-            {student.first_name} {student.last_name}
+            <label>
+              <input type="checkbox" checked={selected.includes(student.id)} onChange={() => toggleSelection(student.id)} />
+              <span>{student.first_name} {student.last_name}</span>
+            </label>
           </li>
         ))}
       </ul>
-      <button>ENROLL</button>
+      {selected.length > 0 && (
+        <button onClick={handleEnroll}>ENROLL</button>
+      )}
     </div>
   )
 };
