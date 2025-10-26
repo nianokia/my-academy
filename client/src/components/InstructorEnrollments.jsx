f59f4c54c4c225464673282c173ab84d1cec6be4
Nia Wright <nw.niawright@gmail.com>
2025-10-26 08:55:20 -0400

    C: add styling to InstructorEnrollments

--- client/src/components/InstructorEnrollments.jsx
+++ client/src/components/InstructorEnrollments.jsx
@@ -77,30 +77,40 @@ const InstructorEnrollments = ({ courseId }) => {
 
   return (
     <div className="InstructorEnrollments">
-      <h2>Instructor Enrollments</h2>
-      <h3>Enrolled Students</h3>
-      <ul style={{ textAlign: 'start' }}>
-        {enrolled.map(student => (
-          <li key={student.id}>
-            {student.first_name} {student.last_name}
-            <button onClick={() => openUnenrollModal(student)}>Unenroll</button>
-          </li>
-        ))}
-      </ul>
-      <h3>Available Students</h3>
-      <ul style={{ textAlign: 'start', listStyle: 'none', margin: '0', padding: '0' }}>
-        {students.map(student => (
-          <li key={student.id}>
-            <label>
-              <input type="checkbox" checked={selected.includes(student.id)} onChange={() => toggleSelection(student.id)} />
+      <h2 style={{ margin: '0.5rem 0rem' }}>Student Enrollment</h2>
+      
+      <div style={{ display: 'flex' }}>
+        <div style={{ width: '50%' }}>
+          <h3 style={{ margin: '0.25rem 0rem 0.75rem' }}>Enrolled Students</h3>
+          <ul style={{ textAlign: 'start', listStyle: 'none', margin: '0', padding: '0', overflowY: 'auto', maxHeight: '10rem', background: 'rgba(0,0,0,0.07)' }}>
+          {enrolled.map(student => (
+            <li key={student.id} style={{ display: 'flex', justifyContent: 'space-between', padding: '0.2rem 1rem'}}>
               <span>{student.first_name} {student.last_name}</span>
-            </label>
-          </li>
-        ))}
-      </ul>
-      {selected.length > 0 && (
-        <button onClick={handleEnroll}>ENROLL</button>
-      )}
+              <button onClick={() => openUnenrollModal(student)}>Unenroll</button>
+            </li>
+          ))}
+        </ul>
+        </div>
+        <div style={{ width: '50%' }}>
+          <h3 style={{ margin: '0.25rem 0rem 0.75rem' }}>Available Students</h3>
+          <ul style={{ textAlign: 'start', listStyle: 'none', margin: '0', padding: '0', overflowY: 'auto', maxHeight: '10rem', background: 'rgba(0,0,0,0.07)', borderRadius: '10px' }}>
+            {students.map(student => (
+              <li key={student.id}>
+                <label style={{ display: 'flex' }}>
+                  <input type="checkbox" 
+                    checked={selected.includes(student.id)}
+                    onChange={() => toggleSelection(student.id)}
+                  />
+                  <span style={{ marginLeft: '-2rem'}}>{student.first_name} {student.last_name}</span>
+                </label>
+              </li>
+            ))}
+          </ul>
+          {selected.length > 0 && (
+            <button onClick={handleEnroll} style={{ marginTop: '1rem' }}>ENROLL</button>
+          )}
+          </div>
+        </div>
 
       <ConfirmModal
         isOpen={isUnenrollModalOpen}
