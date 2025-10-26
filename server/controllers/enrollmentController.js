aa24b5ba9b9acfd210bca4d867d6f7858004292b
Nia Wright <nw.niawright@gmail.com>
2025-10-26 06:03:34 -0400

    S: Add unenrollStudents function & route to server

--- server/controllers/enrollmentController.js
+++ server/controllers/enrollmentController.js
@@ -32,16 +32,40 @@ export const enrollStudents = async (req, res) => {
 
         // --- collect detailed list of all identified students ---
         const students = await User.findAll({ where: { id: studentIds } });
+        if (!students) return res.status(404).json({ message: "No student(s) found" });
+
         // --- addStudents is derived from Course.belongsToMany association ---
         await course.addStudents(students);
 
-        res.status(200).json({ message: "Students enrolled successfully" });
+        res.status(200).json({ message: "Student(s) enrolled successfully" });
     } catch (err) {
-        console.error("Error enrolling students: ", err);
-        res.status(500).json({ message: "Error enrolling students", error: err.message });
+        console.error("Error enrolling student(s): ", err);
+        res.status(500).json({ message: "Error enrolling student(s)", error: err.message });
     }
 };
 
 
 // ------------ DELETE OPERATIONS ------------
 // -------- UNENROLL STUDENTS --------
+export const unenrollStudents = async (req, res) => {
+    try {
+        const { courseId } = req.params;
+        // --- collect array of ids ---
+        const { studentIds } = req.body;
+
+        const course = await Course.findByPk(courseId);
+        if (!course) return res.status(404).json({ message: "Course not found" });
+
+        // --- collect detailed list of all identified students ---
+        const students = await User.findAll({ where: { id: studentIds } });
+        if (!students) return res.status(404).json({ message: "No student(s) found" });
+
+        // --- removeStudents is derived from Course.belongsToMany association ---
+        await course.removeStudents(students);
+
+        res.status(200).json({ message: "Student(s) unenrolled successfully" });
+    } catch (err) {
+        console.error("Error unenrolling student(s): ", err);
+        res.status(500).json({ message: "Error unenrolling student(s)", error: err.message });
+    }
+};
--- server/routes/enrollmentRoutes.js
+++ server/routes/enrollmentRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import verifyToken from '../middleware/authMiddleware.js';
-import { enrollStudents, getEnrolledStudents } from '../controllers/enrollmentController.js';
+import { enrollStudents, getEnrolledStudents, unenrollStudents } from '../controllers/enrollmentController.js';
 
 const router = express.Router();
 
 router.get('/:courseId/students', verifyToken, getEnrolledStudents);
 router.post('/:courseId/enroll', verifyToken, enrollStudents)
+router.delete("/:courseId/unenroll/", verifyToken, unenrollStudents);
 
 export default router;
