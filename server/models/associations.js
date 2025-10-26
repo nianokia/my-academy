81d79fb2dc8c1e0d7b179741cf83d952d69634b9
Nia Wright <nw.niawright@gmail.com>
2025-10-26 02:49:52 -0400

    S: move all  model associations to a single file

---
+++ server/models/associations.js
@@ -0,0 +1,36 @@
+import User from "./User.js";
+import Course from "./Course.js";
+import Enrollment from "./Enrollment.js";
+
+Course.belongsTo(User, { foreignKey: "created_by", as: "instructor" });
+User.hasMany(Course, { foreignKey: "created_by", as: "courses" });
+
+Course.belongsToMany(User, { through: Enrollment, as: "students", foreignKey: "course_id" });
+User.belongsToMany(Course, { through: Enrollment, as: "enrolledCourses", foreignKey: "user_id" });
+
+export { User, Course, Enrollment };
+
+// // --- a user can haave many courses through created_by ---
+// User.hasMany(Course, {
+//     foreignKey: "created_by",
+//     as: "creator",
+// });
+
+// // --- a user can belong to many courses through enrollments ---
+// User.belongsToMany(Course, {
+//     through: 'Enrollments',
+//     as: 'courses',
+//     foreignKey: 'user_id'
+// });
+
+// Course.belongsTo(User, {
+//     foreignKey: "created_by",
+//     as: "creator",
+// });
+
+// // --- a course can belong to many students through enrollments ---
+// Course.belongsToMany(User, {
+//     through: 'Enrollments',
+//     as: 'students',
+//     foreignKey: 'course_id'
+// });
