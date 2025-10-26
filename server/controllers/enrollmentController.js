c029e1bb43ea2bc2a7a490aa198392fc4de91968
Nia Wright <nw.niawright@gmail.com>
2025-10-26 05:02:30 -0400

    S: Add enrollStudents function & route to server

--- server/controllers/enrollmentController.js
+++ server/controllers/enrollmentController.js
@@ -5,16 +5,42 @@ import { User, Course } from "../models/associations.js";
 // ------------ READ OPERATIONS ------------
 // -------- GET ENROLLED STUDENTS FOR COURSE --------
 export const getEnrolledStudents = async (req, res) => {
-  try {
-    const { courseId } = req.params;
-    const course = await Course.findByPk(courseId, {
-      include: { model: User, as: "students", attributes: ["id", "first_name", "last_name", "email"] },
-    });
+    try {
+        const { courseId } = req.params;
+        const course = await Course.findByPk(courseId, {
+            include: { model: User, as: "students", attributes: ["id", "first_name", "last_name", "email"] },
+        });
 
-    if (!course) return res.status(404).json({ message: "Course not found" });
-    res.status(200).json(course.students);
-  } catch (err) {
-    console.error("Error: ", err);
-    res.status(500).json({ message: "Error fetching enrolled students", error: err.message });
-  }
+        if (!course) return res.status(404).json({ message: "Course not found" });
+        res.status(200).json(course.students);
+    } catch (err) {
+        console.error("Error: ", err);
+        res.status(500).json({ message: "Error fetching enrolled students", error: err.message });
+    }
 };
+
+// ------------ POST OPERATIONS ------------
+// -------- ENROLL STUDENTS --------
+export const enrollStudents = async (req, res) => {
+    try {
+        const { courseId } = req.params;
+        // --- collect array of ids ---
+        const { studentIds } = req.body;
+
+        const course = await Course.findByPk(courseId);
+        if (!course) return res.status(404).json({ message: "Course not found" });
+
+        // --- collect detailed list of all identified students ---
+        const students = await User.findAll({ where: { id: studentIds } });
+        // --- addStudents is derived from Course.belongsToMany association ---
+        await course.addStudents(students);
+
+        res.status(200).json({ message: "Students enrolled successfully" });
+    } catch (err) {
+        res.status(500).json({ message: "Error enrolling students", error: err.message });
+    }
+};
+
+
+// ------------ DELETE OPERATIONS ------------
+// -------- UNENROLL STUDENTS --------
--- server/routes/enrollmentRoutes.js
+++ server/routes/enrollmentRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import verifyToken from '../middleware/authMiddleware.js';
-import { getEnrolledStudents } from '../controllers/enrollmentController.js';
+import { enrollStudents, getEnrolledStudents } from '../controllers/enrollmentController.js';
 
 const router = express.Router();
 
 router.get('/:courseId/students', verifyToken, getEnrolledStudents);
+router.post('/:courseId/enroll', verifyToken, enrollStudents)
 
 export default router;
