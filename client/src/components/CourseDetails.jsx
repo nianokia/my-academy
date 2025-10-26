8aab47cdcd055b558027402134d0211a03afcdf9
Nia Wright <nw.niawright@gmail.com>
2025-10-25 23:05:40 -0400

    C: Integrate delete functionality to CourseDetails page

--- client/src/components/CourseDetails.jsx
+++ client/src/components/CourseDetails.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { BackButton } from "../constants/constants";
-import { fetchCourseById } from "../api/course";
+import { fetchCourseById, deleteCourse } from "../api/course";
 import AuthContext from "../context/AuthContext";
 import EditCourse from "./EditCourse";
 import Modal from "./Modal";
@@ -12,6 +12,7 @@ const CourseDetails = () => {
   // --- get ID from the URL ---
   const { courseId } = useParams();
   const { token } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const getCourse = async () => {
     try {
@@ -28,6 +29,21 @@ const CourseDetails = () => {
     getCourse();
   }, [courseId, token]);
 
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    try {
+      await deleteCourse(courseId, token);
+      alert(`${course.name} has been deleted successfully!`);
+      // console.log(`${deletedCourse.name} was successfully deleted \n ID: ${deletedCourse.id}`)
+      
+      navigate(-1)
+    } catch (err) {
+      alert('Failed to delete course. Please try again.');
+      // console.error('Error deleting course:', err.name, '\n', err.message, '\n Url:', err.config?.url);
+      console.error('Error deleting course: ', err)
+    }
+  }
+
   // --- Close modal & reset role ---
   const handleCloseModal = () => {
     setIsModalOpen(false);
@@ -40,23 +56,26 @@ const CourseDetails = () => {
       {!course ? (
         <p>Course not found.</p>
       ) : (
-        <ul className='courseList'>
-          <li key={course.id} value={course.id} className="singleCourse">
-            <div style={{ display: 'flex', justifyContent: 'space-between', margin: '0.5rem auto' }}>
-              <h3 style={{ margin: '0' }}>{course.name}</h3>
-              <button className="editProfileIcon" onClick={() => setIsModalOpen(true)}>✎</button>
-            </div>
-            <ul>
-              <li>ID: {course.id}</li>
-              <li>Credits: {course.credits}</li>
-              <li>Enrollment Limit: {course.enrollment_limit}</li>
-              <li>Created by: {course.created_by}</li>
-              <li>Created at: {course.created_at}</li>
-            </ul>
-          </li>
-        </ul>
+        <>
+          <ul className='courseList'>
+            <li key={course.id} value={course.id} className="singleCourse">
+              <div style={{ display: 'flex', justifyContent: 'space-between', margin: '0.5rem auto' }}>
+                <h3 style={{ margin: '0' }}>{course.name}</h3>
+                <button className="editProfileIcon" onClick={() => setIsModalOpen(true)}>✎</button>
+              </div>
+              <ul>
+                <li>ID: {course.id}</li>
+                <li>Credits: {course.credits}</li>
+                <li>Enrollment Limit: {course.enrollment_limit}</li>
+                <li>Created by: {course.created_by}</li>
+                <li>Created at: {course.created_at}</li>
+              </ul>
+            </li>
+          </ul>
+          <button className="deleteUserBtn" onClick={handleDelete}>Delete User</button>
+        </>
       )}
-      
+
       {/* -------- EDIT COURSE MODAL -------- */}
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
         {/* --- Render the appropriate registration form based on the selected role --- */}
