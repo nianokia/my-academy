969ba874feb45df5d7cf777f7e2477b3641a48fe
Nia Wright <nw.niawright@gmail.com>
2025-10-26 04:46:25 -0400

    C: add fetchUsers API helper function to user.jsx

--- client/src/api/user.jsx
+++ client/src/api/user.jsx
@@ -4,19 +4,28 @@ import axios from "axios";
 const API_URL = `${import.meta.env.VITE_DOMAIN}/api/user`;
 console.log('API URL:', API_URL);
 
-export const updateUser = async (userId, updatedData, token) => {
-    const res = await axios.put(
-        `${API_URL}/${userId}`,
-        updatedData, {
-            headers: { Authorization: `Bearer ${token}` },
-        }
-    );
-    return res.data;
+// -------- FETCH ALL USERS --------
+export const fetchUsers = async (token) => {
+  const headers = token ? { Authorization: `Bearer ${token}` } : {};
+  const res = await axios.get(API_URL, { headers });
+  return res.data;
 };
 
+// -------- UPDATE USER -------
+export const updateUser = async (userId, updatedData, token) => {
+  const res = await axios.put(
+    `${API_URL}/${userId}`,
+    updatedData, {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+  return res.data;
+};
+
+// -------- DELETE USER --------
 export const deleteUser = async (userId, token) => {
-    const res = await axios.delete(`${API_URL}/${userId}`, {
-        headers: {Authorization: `Bearer ${token}`},
-    });
-    return res.data;
+  const res = await axios.delete(`${API_URL}/${userId}`, {
+    headers: {Authorization: `Bearer ${token}`},
+  });
+  return res.data;
 }
