bc65ef2a7787fcebe33529e3b6b4a49b8333dc77
Nia Wright <nw.niawright@gmail.com>
2025-10-27 09:40:06 -0400

    S: import all functions from gradeController to gradeRoutes

--- server/routes/gradeRoutes.js
+++ server/routes/gradeRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import verifyToken from "../middleware/authMiddleware.js";
-import { assignGrade } from "../controllers/gradeController.js";
+import { assignGrade, getStudentGrades, getInstructorGrades } from "../controllers/gradeController.js";
 
 const router = express.Router();
 
