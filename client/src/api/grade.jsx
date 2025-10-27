37d318e3e0696777810c3c8ca2cc29cd9f0ab21e
Nia Wright <nw.niawright@gmail.com>
2025-10-27 09:47:57 -0400

    C: Add grade API helper functions for each route

---
+++ client/src/api/grade.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// -------- DEFINE USER API BASE URL --------
+const API_URL = `${import.meta.env.VITE_DOMAIN}/api/enrollments`;
+console.log('GRADE API URL:', API_URL);
+
+// ------------ GET OPERATIONS ------------
+// -------- FETCH STUDENT'S GRADES --------
+export const fetchStudentGrades = async (studentId, token) => {
+  const res = await axios.get(`${API_URL}/student/${studentId}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
+
+// -------- FETCH INSTRUCTOR'S ASSIGNED GRADES --------
+export const fetchInstructorGrades = async (token) => {
+  const res = await axios.get(`${API_URL}/instructor`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
+
+// ------------ CREATE OPERATIONS ------------
+// -------- ASSIGN GRADE --------
+export const assignGrade = async (enrollmentId, grade, token) => {
+  const res = await axios.post(`${API_URL}/assign/${enrollmentId}`, { grade }, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
